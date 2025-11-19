4e19ad7965edcbd05c1514457ea29a35db66e9da
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-11-19 20:28:56 +0700

    CRUD hospital: specialty admin controller

--- src/main/java/com/hieunt/medlink/app/controllers/admin/AdminSpecialtiesController.java
+++ src/main/java/com/hieunt/medlink/app/controllers/admin/AdminSpecialtiesController.java
@@ -5,6 +5,9 @@ import com.hieunt.medlink.app.requests.hospital.SpecialtyRequest;
 import com.hieunt.medlink.app.responses.BaseResponse;
 import com.hieunt.medlink.app.services.hospital.SpecialtiesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,5 +30,16 @@ public class AdminSpecialtiesController {
     @DeleteMapping("/{id}")
     public ResponseEntity<BaseResponse<SpecialtiesEntity>> deleteSpecialty(@PathVariable Long id) {
         return ResponseEntity.ok(specialtiesService.deleteSpecialty(id));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BaseResponse<SpecialtiesEntity>> getSpecialtyById(@PathVariable Long id) {
+        return ResponseEntity.ok(specialtiesService.getSpecialtyById(id));
+    }
+
+    @GetMapping("/{hospitalId}")
+    public ResponseEntity<BaseResponse<Page<SpecialtiesEntity>>> filterSpecialties(@PathVariable Long hospitalId, @PageableDefault(size = 20) Pageable pageable) {
+        BaseResponse<Page<SpecialtiesEntity>> specialties = specialtiesService.filterSpecialties(hospitalId, pageable);
+        return ResponseEntity.ok(specialties);
     }
 }
