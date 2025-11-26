e10f46acc9b7c6a97fa83a654a045bc3e0bbe301
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-11-26 21:32:09 +0700

    room: patient controller

---
+++ src/main/java/com/hieunt/medlink/app/controllers/patitent/PatientRoomController.java
@@ -0,0 +1,28 @@
+package com.hieunt.medlink.app.controllers.patitent;
+
+import com.hieunt.medlink.app.entities.RoomEntity;
+import com.hieunt.medlink.app.responses.BaseResponse;
+import com.hieunt.medlink.app.services.hospital.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/patient/hospital/rooms")
+@RequiredArgsConstructor
+public class PatientRoomController {
+    private final RoomService roomService;
+
+    @GetMapping("/{hospitalId}")
+    public ResponseEntity<BaseResponse<Page<RoomEntity>>> filterRooms(@PathVariable Long hospitalId,
+            @PageableDefault(size = 20) Pageable pageable) {
+        BaseResponse<Page<RoomEntity>> rooms = roomService.filterRooms(hospitalId, pageable);
+        return ResponseEntity.ok(rooms);
+    }
+}
