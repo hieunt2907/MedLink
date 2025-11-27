5028c8c2dd8c21b61462d6144dbb4a8dba70fc31
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-11-27 11:10:20 +0700

    doctor: profile dto request

---
+++ src/main/java/com/hieunt/medlink/app/requests/doctor/DoctorProfileRequest.java
@@ -0,0 +1,15 @@
+package com.hieunt.medlink.app.requests.doctor;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorProfileRequest {
+    private Long userId;
+    private Long specialtyId;
+    private String qualifications;
+    private Integer experienceYear;
+}
