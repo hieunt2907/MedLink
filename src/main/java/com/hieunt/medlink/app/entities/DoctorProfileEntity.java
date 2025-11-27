9a8baf8ae19a574a557bd0b28809f4bd0c88440b
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-11-27 11:06:02 +0700

    doctor: entity profile

---
+++ src/main/java/com/hieunt/medlink/app/entities/DoctorProfileEntity.java
@@ -0,0 +1,34 @@
+package com.hieunt.medlink.app.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "doctor_profiles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorProfileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "specialty_id")
+    private Long specialtyId;
+
+    @Column(name = "qualifications", columnDefinition = "TEXT")
+    private String qualifications;
+
+    @Column(name = "experience_year")
+    private Integer experienceYear;
+}
