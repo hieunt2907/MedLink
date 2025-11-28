34de408e7c493c5efc7699300c0d17e74851324e
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-11-28 23:35:20 +0700

    doctor room assignment: request

---
+++ src/main/java/com/hieunt/medlink/app/requests/doctor/DoctorRoomAssignmentsRequest.java
@@ -0,0 +1,25 @@
+package com.hieunt.medlink.app.requests.doctor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorRoomAssignmentsRequest {
+    private Long doctorProfileId;
+    private Long hospitalId;
+    private Long roomId;
+    private Long specialtyId;
+    private Boolean isPrimary;
+    private Short[] daysOfWeek;
+    private LocalTime shiftStart;
+    private LocalTime shiftEnd;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String note;
+}
