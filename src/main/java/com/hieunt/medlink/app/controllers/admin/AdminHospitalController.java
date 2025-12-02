f205713d3891eebfb14a1fd56b7237d9d2c3ec7d
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-12-02 21:18:17 +0700

    hospital: admin controller

---
+++ src/main/java/com/hieunt/medlink/app/controllers/admin/AdminHospitalController.java
@@ -0,0 +1,28 @@
+package com.hieunt.medlink.app.controllers.admin;
+
+import com.hieunt.medlink.app.entities.HospitalEntity;
+import com.hieunt.medlink.app.responses.BaseResponse;
+import com.hieunt.medlink.app.services.hospital.HospitalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin/hospital")
+@RequiredArgsConstructor
+public class AdminHospitalController {
+    private final HospitalService hospitalService;
+
+    @GetMapping("/")
+    public ResponseEntity<BaseResponse<Page<HospitalEntity>>> filterHospitals(@RequestParam(required = false) String keyword, @PageableDefault(size = 20) Pageable pageable) {
+        BaseResponse<Page<HospitalEntity>> response = hospitalService.filterHospitals(keyword, pageable);
+        return ResponseEntity.ok(response);
+    }
+
+}
