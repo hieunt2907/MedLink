2b834f52d1bf3413787103fb15fc0202832cf395
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-12-08 20:19:48 +0700

    doctor: room service

---
+++ src/main/java/com/hieunt/medlink/app/services/doctor/DoctorRoomAssignmentsService.java
@@ -0,0 +1,20 @@
+package com.hieunt.medlink.app.services.doctor;
+
+import com.hieunt.medlink.app.entities.DoctorRoomAssignmentsEntity;
+import com.hieunt.medlink.app.requests.doctor.DoctorRoomAssignmentsRequest;
+import com.hieunt.medlink.app.responses.BaseResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface DoctorRoomAssignmentsService {
+    BaseResponse<DoctorRoomAssignmentsEntity> createDoctorRoomAssignment(DoctorRoomAssignmentsRequest request);
+
+    BaseResponse<DoctorRoomAssignmentsEntity> updateDoctorRoomAssignment(Long id, DoctorRoomAssignmentsRequest request);
+
+    BaseResponse<DoctorRoomAssignmentsEntity> getDoctorRoomAssignmentById(Long id);
+
+    BaseResponse<DoctorRoomAssignmentsEntity> deleteDoctorRoomAssignment(Long id);
+
+    BaseResponse<Page<DoctorRoomAssignmentsEntity>> filterDoctorRoomAssignments(Long doctorProfileId, String keyword,
+            Pageable pageable);
+}
