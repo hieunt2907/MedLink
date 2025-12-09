7852f177f7a832b7a175c920f46be968848215fa
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-12-09 20:30:08 +0700

    doctor room assignment: controller doctor

---
+++ src/main/java/com/hieunt/medlink/app/controllers/doctor/DoctorDoctorRoomAssignmentsController.java
@@ -0,0 +1,31 @@
+package com.hieunt.medlink.app.controllers.doctor;
+
+import com.hieunt.medlink.app.entities.DoctorRoomAssignmentsEntity;
+import com.hieunt.medlink.app.responses.BaseResponse;
+import com.hieunt.medlink.app.services.doctor.DoctorRoomAssignmentsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/doctor/doctors/room-assignments")
+@RequiredArgsConstructor
+public class DoctorDoctorRoomAssignmentsController {
+    private final DoctorRoomAssignmentsService doctorRoomAssignmentsService;
+
+    @GetMapping("/filter")
+    public ResponseEntity<BaseResponse<Page<DoctorRoomAssignmentsEntity>>> filterDoctorRoomAssignments(
+            @RequestParam(required = false) Long doctorProfileId,
+            @RequestParam(required = false, defaultValue = "") String keyword,
+            @PageableDefault(size = 20) Pageable pageable) {
+        BaseResponse<Page<DoctorRoomAssignmentsEntity>> assignments = doctorRoomAssignmentsService
+                .filterDoctorRoomAssignments(doctorProfileId, keyword, pageable);
+        return ResponseEntity.ok(assignments);
+    }
+}
