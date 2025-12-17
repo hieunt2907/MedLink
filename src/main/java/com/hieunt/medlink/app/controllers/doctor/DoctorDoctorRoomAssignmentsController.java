ebf461e2ab0ee7620374bc122674979ae122a9e7
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-12-17 20:23:30 +0700

    doctor doctor room assignment controller

--- src/main/java/com/hieunt/medlink/app/controllers/doctor/DoctorDoctorRoomAssignmentsController.java
+++ src/main/java/com/hieunt/medlink/app/controllers/doctor/DoctorDoctorRoomAssignmentsController.java
@@ -1,7 +1,9 @@
 package com.hieunt.medlink.app.controllers.doctor;
 
 import com.hieunt.medlink.app.entities.DoctorRoomAssignmentsEntity;
+
 import com.hieunt.medlink.app.responses.BaseResponse;
+import com.hieunt.medlink.app.responses.doctor.DoctorRoomAssignmentResponse;
 import com.hieunt.medlink.app.services.doctor.DoctorRoomAssignmentsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -20,11 +22,11 @@ public class DoctorDoctorRoomAssignmentsController {
     private final DoctorRoomAssignmentsService doctorRoomAssignmentsService;
 
     @GetMapping("/filter")
-    public ResponseEntity<BaseResponse<Page<DoctorRoomAssignmentsEntity>>> filterDoctorRoomAssignments(
+    public ResponseEntity<BaseResponse<Page<DoctorRoomAssignmentResponse>>> filterDoctorRoomAssignments(
             @RequestParam(required = false) Long doctorProfileId,
             @RequestParam(required = false, defaultValue = "") String keyword,
             @PageableDefault(size = 20) Pageable pageable) {
-        BaseResponse<Page<DoctorRoomAssignmentsEntity>> assignments = doctorRoomAssignmentsService
+        BaseResponse<Page<DoctorRoomAssignmentResponse>> assignments = doctorRoomAssignmentsService
                 .filterDoctorRoomAssignments(doctorProfileId, keyword, pageable);
         return ResponseEntity.ok(assignments);
     }
