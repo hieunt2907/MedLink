48bb219c770c8f93d6afc598b573a7b157729895
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-12-21 21:01:40 +0700

    hospital repository

--- src/main/java/com/hieunt/medlink/app/repositories/HospitalRepository.java
+++ src/main/java/com/hieunt/medlink/app/repositories/HospitalRepository.java
@@ -9,20 +9,13 @@ public interface HospitalRepository extends BaseRepository<HospitalEntity, Long>
     boolean existsByName(String name);
 
     @Query(value = """
-                select
-                    h.id          as id,
-                    h.name        as name,
-                    h.address     as address,
-                    h.phone_number as phoneNumber,
-                    h.email       as email,
-                    h.description as description
+                select h.*
                 from hospitals h
                 where
                     h.name ilike concat('%', :keyword, '%')
                     or h.address ilike concat('%', :keyword, '%')
                     or h.phone ilike concat('%', :keyword, '%')
                     or h.email ilike concat('%', :keyword, '%')
-                    or h.website ilike concat('%', :keyword, '%')
                     and h.status = 'active'
             """, nativeQuery = true)
     Page<HospitalEntity> filterHospitals(String keyword, Pageable pageable);
