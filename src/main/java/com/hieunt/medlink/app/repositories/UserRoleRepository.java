2c1402c8418ef6518c83d3a915553ca33ae9ba53
Nguyễn Trung Hiếu <hieu29072004@gmail.com>
2025-12-21 21:03:52 +0700

    user role repository

--- src/main/java/com/hieunt/medlink/app/repositories/UserRoleRepository.java
+++ src/main/java/com/hieunt/medlink/app/repositories/UserRoleRepository.java
@@ -4,5 +4,6 @@ import com.hieunt.medlink.app.entities.UserRoleEntity;
 
 public interface UserRoleRepository extends BaseRepository<UserRoleEntity, Long> {
     void deleteByUserId(Long userId);
+
     UserRoleEntity findByUserId(Long userId);
 }
