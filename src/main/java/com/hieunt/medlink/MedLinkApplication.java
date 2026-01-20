70cff2b56a95c6e961c444ef393cef056a9e4333
hieunt <hieu29072004@gmail.com>
2026-01-20 21:28:56 +0700

    commit

---
+++ src/main/java/com/hieunt/medlink/MedLinkApplication.java
@@ -0,0 +1,13 @@
+package com.hieunt.medlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedLinkApplication.class, args);
+    }
+
+}
