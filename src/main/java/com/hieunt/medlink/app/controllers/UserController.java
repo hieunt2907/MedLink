3098493d5901e81ea2f2c93895d11ec9052f414d
hieunt <hieu29072004@gmail.com>
2026-01-20 21:29:13 +0700

    commit

--- src/main/java/com/hieunt/medlink/app/controllers/UserController.java
+++ src/main/java/com/hieunt/medlink/app/controllers/UserController.java
@@ -5,8 +5,6 @@ import com.hieunt.medlink.app.responses.BaseResponse;
 import com.hieunt.medlink.app.responses.user.UserResponse;
 import com.hieunt.medlink.app.services.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,16 +19,6 @@ public class UserController {
     public ResponseEntity<BaseResponse<UserResponse>> createUser(@RequestBody UserCreateRequest user) {
         try {
             BaseResponse<UserResponse> response = userService.createUser(user);
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new BaseResponse<>(e.getMessage(), null));
-        }
-    }
-
-    @GetMapping("/filter")
-    public ResponseEntity<BaseResponse<Page<UserResponse>>> filterUsers(@RequestParam(required = false) String keyword, Pageable pageable) {
-        try {
-            BaseResponse<Page<UserResponse>> response = userService.filterUsers(keyword, pageable);
             return ResponseEntity.ok(response);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new BaseResponse<>(e.getMessage(), null));
