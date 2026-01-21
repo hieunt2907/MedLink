14eb3cc03c4e084006332d9707af0503dbb94308
hieunt <hieu29072004@gmail.com>
2026-01-21 23:06:08 +0700

    commit

--- src/main/java/com/hieunt/medlink/app/controllers/doctor/DoctorLabOrdersController.java
+++ src/main/java/com/hieunt/medlink/app/controllers/doctor/DoctorLabOrdersController.java
@@ -42,12 +42,6 @@ public class DoctorLabOrdersController {
         return ResponseEntity.ok(labOrdersService.getLabOrderByOrderNumber(orderNumber));
     }
 
-    @GetMapping("/medical-record/{medicalRecordId}")
-    public ResponseEntity<BaseResponse<Page<LabOrdersEntity>>> getLabOrdersByMedicalRecordId(
-            @PathVariable Long medicalRecordId,
-            @PageableDefault(size = 20) Pageable pageable) {
-        return ResponseEntity.ok(labOrdersService.getLabOrdersByMedicalRecordId(medicalRecordId, pageable));
-    }
 
     @GetMapping("/my-orders")
     public ResponseEntity<BaseResponse<Page<LabOrdersEntity>>> getMyDoctorLabOrders(
