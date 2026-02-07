44ccfc0cf9bdaa24f53b357301f0dde780980bab
hieunt <hieu29072004@gmail.com>
2026-02-07 22:30:14 +0700

    commit

--- src/main/java/com/hieunt/medlink/app/services/patient/PatientAllergiesServiceImpl.java
+++ src/main/java/com/hieunt/medlink/app/services/patient/PatientAllergiesServiceImpl.java
@@ -3,13 +3,14 @@ package com.hieunt.medlink.app.services.patient;
 import com.hieunt.medlink.app.entities.PatientAllergiesEntity;
 import com.hieunt.medlink.app.entities.PatientProfilesEntity;
 import com.hieunt.medlink.app.entities.UserEntity;
+import com.hieunt.medlink.app.errors.ResourceNotFoundException;
 import com.hieunt.medlink.app.mappers.PatientAllergiesMapper;
 import com.hieunt.medlink.app.repositories.PatientAllergiesRepository;
 import com.hieunt.medlink.app.repositories.PatientProfileRepository;
 import com.hieunt.medlink.app.requests.patient.PatientAllergiesRequest;
 import com.hieunt.medlink.app.responses.BaseResponse;
-import com.hieunt.medlink.pkg.error.ResourceNotFoundException;
-import com.hieunt.medlink.pkg.utils.GetCurrentUser;
+import com.hieunt.medlink.app.utils.GetCurrentUser;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
