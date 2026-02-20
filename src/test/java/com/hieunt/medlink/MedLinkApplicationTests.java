c6725178f5069fcf45d3c280a282c912ce101e8e
hieunt <hieu29072004@gmail.com>
2026-02-20 21:19:22 +0700

    commit

---
+++ src/test/java/com/hieunt/medlink/MedLinkApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.hieunt.medlink;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MedLinkApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
